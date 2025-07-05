5d54cf140427f9000201a0a7a0762f60d1da515d
Lucas Pluchon <lucas.pluchon@epita.fr>
2025-07-05 16:43:37 +0200

    fixed rpc ?

--- src/mappers/creatorbid/effect.creatorbid.ts
+++ src/mappers/creatorbid/effect.creatorbid.ts
@@ -1,17 +1,18 @@
 import {experimental_createEffect, S} from "envio";
-import {client} from "../../utils/rpc/client.rpc";
+import {getClient} from "../../utils/rpc/client.rpc";
 
 export const getCreatorBidBondingCurveEffect = experimental_createEffect(
     {
         name: "getCreatorBidBondingCurve",
         input: {
             tokenAddress: S.string,
+            chainId: S.number,
         },
         output: S.optional(S.string),
     },
     async ({ input, context }) => {
         try {
-            const result = await client.readContract({
+            const result = await getClient(input.chainId).readContract({
                 address: input.tokenAddress as `0x${string}`,
                 abi: [
                     {
